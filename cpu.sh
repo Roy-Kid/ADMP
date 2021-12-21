48ef6e060a1d23d17118d3442a35e44da1a586e9
Roy Kid <lijichen365@126.com>
2021-12-21 16:28:24 +0800

    remove old files

--- cpu.sh
+++
@@ -1,8 +0,0 @@
-#!/bin/bash
-#SBATCH --export=ALL
-#SBATCH --job-name=run_jax_gpu
-#SBATCH -o cpu.out -e cpu.err
-#SBATCH -N 1 -n 4 -t 00:10:00 --mem 10000mb -c 1
-#SBATCH -p debug
-
-python demo.py
--- test_jax.py
+++
@@ -1,3 +0,0 @@
-import jax
-print(jax.devices())
-print(jax.numpy.array([1., 2.,])
